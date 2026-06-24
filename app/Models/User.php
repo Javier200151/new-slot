ea1f916553513350a119247edb44b2a5182042ba
javier200151 <javier200151@gmail.com>
2026-06-24 12:05:07 +0200

    Filament 5 panel creado y primer usuario admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,29 +4,57 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
+//use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Models\Contracts\HasName;
+use Filament\Panel;
 
-#[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser, HasName
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     /**
      * Get the attributes that should be cast.
      *
      * @return array<string, string>
      */
+
+    protected $fillable = [
+        'nick',
+        'email',
+        'password',
+        'promo_id',
+        'tagname',
+        'status_id',
+        'firma',
+        'arma_uid',
+        'discord_id',
+        'steam_id',
+        'member_at',
+        'created_by',
+        'updated_by',
+    ];
+
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
+    public function getFilamentName(): string
+    {
+        return $this->nick ?? $this->email;
+    }
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
+
 }
