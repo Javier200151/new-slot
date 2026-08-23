7f19f9b5b2d74677829c1bb7f8318cad9ccb38b8
javier200151 <javier200151@gmail.com>
2026-08-23 21:47:31 +0200

    ajustado envio de correo

---
+++ app/Notifications/VerifyEmailNotification.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Auth\Notifications\VerifyEmail as BaseVerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class VerifyEmailNotification extends BaseVerifyEmail
+{
+    public function toMail(object $notifiable): MailMessage
+    {
+        $verificationUrl = $this->verificationUrl($notifiable);
+
+        return (new MailMessage)
+            ->subject('Verifica tu correo electrónico | Squad ALPHA')
+            ->view(
+                'emails.verify-email',
+                [
+                    'user' => $notifiable,
+                    'verificationUrl' => $verificationUrl,
+                ],
+            );
+    }
+}
