1a5ca486dfd1e0e67ad88ecbf23ccf7fc88142e8
javier200151 <javier200151@gmail.com>
2026-08-23 17:47:23 +0200

    inicio de la API

---
+++ config/external_api.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Token API externa
+    |--------------------------------------------------------------------------
+    |
+    | El token real se almacena en .env.
+    | Nunca debe almacenarse en Git.
+    |
+    */
+
+    'token' => env('EXTERNAL_API_TOKEN'),
+
+];
